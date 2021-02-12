59f238138622770f62aedb9af17ca6e84c7e6690
Mary Baker <meb601@gmail.com>
2021-02-12 15:52:06 -0500

    Remove README.md create-react-app notes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Surf Break Costa Rica</h1>
+    <div style={{display: "flex", alignItems: "center", padding: "1rem"}}>
+      <img src="./surf-break-costa-rica.png" alt="Surf Break Costa Rica Logo"width="150" height="150"></img>
+      <h1 style={{padding: "1rem"}}>Your Blank Canvas Awaits</h1>
     </div>
   );
-}
+};
 
 export default App;
