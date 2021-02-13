6c1795b115b28062506041e6a8496141f2fbf635
Mary Baker <meb601@gmail.com>
2021-02-13 14:20:57 -0500

    Add react form hook functionality.

--- src/Login.js
+++ src/Login.js
@@ -22,21 +22,21 @@ const Login = (props) => {
                             </a>
                         </p>
                     </div>
-                    <form class="mt-8 space-y-6" action="#" method="POST">
+                    <form class="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit(onSubmit)}>
                         <input type="hidden" name="remember" value="true"></input>
                         <div class="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label for="email-address" class="sr-only">Email address</label>
-                                <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"></input>
+                                <input ref={register} id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"></input>
                             </div>
                             <div>
                                 <label for="password" class="sr-only">Password</label>
-                                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"></input>
+                                <input ref={register} id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"></input>
                             </div>
                         </div>
                         <div class="flex items-center justify-between">
                             <div class="flex items-center">
-                                <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-turquoise-dark focus:ring-turquoise-dark border-gray-300 rounded"></input>
+                                <input ref={register} id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-turquoise-dark focus:ring-turquoise-dark border-gray-300 rounded"></input>
                                 <label for="remember_me" class="ml-2 block text-sm text-gray-900">
                                     Remember me
                                 </label>
