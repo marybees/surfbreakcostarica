acdd639666076704649c48f1e03af63c43202374
Mary Baker <meb601@gmail.com>
2021-02-14 16:04:58 -0500

    Add progress bar component.

---
+++ src/components/client/CheckListProgress.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CheckListProgress() {
+    return (
+        <div>
+            <div class="bg-white p-8 px-16">
+                <div class="text-gray-darkest text-xs">Pre-Trip Checklist Progress</div>
+                <div class="mt-2 bg-gray rounded-full">
+                    <div class="w-8/12 mt-2 bg-turquoise py-1 text-center rounded-full"><div class="text-white text-sm inline-block bg-turquoise-dark px-2 rounded-full">75%</div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CheckListProgress
--- src/components/client/ClientHome.js
+++ src/components/client/ClientHome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ClientNav from './ClientNav';
+import CheckListProgress from './CheckListProgress';
 
 export const ClientHome = (props) => {
     return (
@@ -14,45 +15,45 @@ export const ClientHome = (props) => {
                 </svg>
                 {/* Don't remove this div. It's a placeholder for where the former navbar was. */}
                 <div class="relative pt-6 px-4 sm:px-6 lg:px-8"></div>
-                <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <main class="mt-8 mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-14 lg:mt-18 lg:px-8 xl:mt-26">
                     <div class="sm:text-center lg:text-left">
-                    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    <h1 class="text-4xl tracking-tight font-extrabold text-gray-darkest sm:text-5xl md:text-6xl">
                         <div class="block">Hola, <span class="text-turquoise">Mary</span></div>
                     </h1>
-                    <p class="text-1xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl pt-6">There are <span class="text-turquoise">10</span> days left until your surf adventure!</p>
-                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                    <p class="text-1xl tracking-tight font-extrabold text-gray-darkest sm:text-2xl md:text-3xl pt-6">There are <span class="text-turquoise">10</span> days left until your surf adventure!</p>
+                    <p class="mt-3 text-base text-gray-darkest sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                         We can't wait to see you in beautiful Costa Rica! Before then, be sure to complete your pre-trip checklist.
                     </p>
-                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">See you soon and pura vida!</p>
-                    <p class="text-1xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl py-6">Pre-Trip Checklist</p>
+                    <p class="mt-3 text-base text-gray-darkest sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">See you soon and pura vida!</p>
+                    <p class="text-1xl tracking-tight font-extrabold text-gray-darkest sm:text-2xl md:text-3xl py-6">Pre-Trip Checklist</p>
                     <div>
                         <label class="flex justify-start items-start">
                             <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
                                 <input type="checkbox" class="text-turquoise"></input>
                                 <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
                             </div>
-                            <div class="select-none">Send us your flight info</div>
+                            <div class="select-none text-sm">Send us your flight info</div>
                         </label>
                         <label class="flex justify-start items-start">
                             <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
                                 <input type="checkbox" class="text-turquoise"></input>
                                 <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
                             </div>
-                            <div class="select-none">Sign our release forms</div>
+                            <div class="select-none text-sm">Sign our release forms</div>
                         </label>
                         <label class="flex justify-start items-start">
                             <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
                                 <input type="checkbox" class="text-turquoise"></input>
                                 <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
                             </div>
-                            <div class="select-none">Get packing with our Getting Ready for Your Trip blog post</div>
+                            <div class="select-none text-sm">Get packing with our suggested list</div>
                         </label>
                         <label class="flex justify-start items-start">
                             <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
                                 <input type="checkbox" class="text-turquoise"></input>
                                 <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
                             </div>
-                            <div class="select-none">Save our WhatsApp number to your phone</div>
+                            <div class="select-none text-sm">Save our WhatsApp number to your phone</div>
                         </label>
                     </div>
                     </div>
@@ -63,6 +64,7 @@ export const ClientHome = (props) => {
                 <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1414490929659-9a12b7e31907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80" alt="woman with surfboard at sunset"></img>
             </div>
             </div>
+            <CheckListProgress />
         </div>
     )
 }
