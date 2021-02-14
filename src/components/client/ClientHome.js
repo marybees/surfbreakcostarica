902cc8e1073a5e3d0793d3069806baf05779521b
Mary Baker <meb601@gmail.com>
2021-02-14 14:35:42 -0500

    Include Tailwind hero code in client dahboard home.

--- src/components/client/ClientHome.js
+++ src/components/client/ClientHome.js
@@ -6,6 +6,63 @@ export const ClientHome = (props) => {
     return (
         <div>
             <ClientNav />
+            <div class="relative bg-white overflow-hidden">
+            <div class="max-w-7xl mx-auto">
+                <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+                    <polygon points="50,0 100,0 50,100 0,100" />
+                </svg>
+                {/* Don't remove this div. It's a placeholder for where the former navbar was. */}
+                <div class="relative pt-6 px-4 sm:px-6 lg:px-8"></div>
+                <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                    <div class="sm:text-center lg:text-left">
+                    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                        <div class="block">Hola, <span class="text-turquoise">Mary</span></div>
+                    </h1>
+                    <p class="text-1xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl pt-6">There are <span class="text-turquoise">10</span> days left until your surf adventure!</p>
+                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                        We can't wait to see you in beautiful Costa Rica! Before then, be sure to complete your pre-trip checklist.
+                    </p>
+                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">See you soon and pura vida!</p>
+                    <p class="text-1xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl py-6">Pre-Trip Checklist</p>
+                    <div>
+                        <label class="flex justify-start items-start">
+                            <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
+                                <input type="checkbox" class="text-turquoise"></input>
+                                <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+                            </div>
+                            <div class="select-none">Send us your flight info</div>
+                        </label>
+                        <label class="flex justify-start items-start">
+                            <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
+                                <input type="checkbox" class="text-turquoise"></input>
+                                <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+                            </div>
+                            <div class="select-none">Sign our release forms</div>
+                        </label>
+                        <label class="flex justify-start items-start">
+                            <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
+                                <input type="checkbox" class="text-turquoise"></input>
+                                <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+                            </div>
+                            <div class="select-none">Get packing with our Getting Ready for Your Trip blog post</div>
+                        </label>
+                        <label class="flex justify-start items-start">
+                            <div class="bg-white border-2 rounded border-gray w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-gray">
+                                <input type="checkbox" class="text-turquoise"></input>
+                                <svg class="fill-current hidden w-4 h-4 text-turquoise pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+                            </div>
+                            <div class="select-none">Save our WhatsApp number to your phone</div>
+                        </label>
+                    </div>
+                    </div>
+                </main>
+                </div>
+            </div>
+            <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1414490929659-9a12b7e31907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80" alt="woman with surfboard at sunset"></img>
+            </div>
+            </div>
         </div>
     )
 }
--- src/components/client/ClientNav.js
+++ src/components/client/ClientNav.js
@@ -7,6 +7,7 @@ export default function ClientNav() {
                 <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div class="relative flex items-center justify-between h-16">
                         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            {/* Hamburger Menu */}
                             <button class="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
                                 <span class="sr-only">Open main menu</span>
                                 <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -18,10 +19,12 @@ export default function ClientNav() {
                             </button>
                         </div>
                         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                            {/* Logo */}
                             <div class="flex-shrink-0 flex items-center">
                                 <img class="block lg:hidden h-8 w-auto" src="./sbcr-logo-transparent-white-bg.png" alt="Surf Break Costa Rica Logo"></img>
                                 <img class="hidden lg:block h-8 w-auto" src="./sbcr-logo-transparent-white-bg.png" alt="Surf Break Costa Rica logo"></img>
                             </div>
+                            {/* Nav Items */}
                             <div class="hidden sm:block sm:ml-6">
                                 <div class="flex space-x-4">
                                     <a href="/home" class="bg-turquoise text-white hover:bg-turquoise-dark px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
@@ -33,26 +36,23 @@ export default function ClientNav() {
                             </div>
                         </div>
                         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            {/* Notification Icon */}
                             <button class="bg-turquoise p-1 rounded-full text-white hover:bg-turquoise-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                 <span class="sr-only">View notifications</span>
                                 <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                                 </svg>
                             </button>
-                            <div class="ml-3 relative">
-                                <div>
-                                    <button class="bg-blue flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-white" id="user-menu" aria-haspopup="true"></button>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="hidden sm:hidden">
                     <div class="px-2 pt-2 pb-3 space-y-1">
-                        <a href="#" class="bg-turquoise text-white hover:bg-turquoise-dark block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
-                        <a href="#" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-                        <a href="#" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-                        <a href="#" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                        <a href="/" class="bg-blue text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
+                        <a href="/flightinfo" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Flight Info</a>
+                        <a href="/releaseforms" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Release Forms</a>
+                        <a href="/itinerary" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Itinerary</a>
+                        <a href="/photos" class="text-white hover:bg-turquoise hover:text-white block px-3 py-2 rounded-md text-base font-medium">Photos</a>
                     </div>
                 </div>
             </nav>
