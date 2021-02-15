f2673d62efe1d1e499b81ddec421f43eb9c72f28
Mary Baker <meb601@gmail.com>
2021-02-15 12:14:59 -0500

    Create handleOnChange and install npm package for international phone numbers.

--- src/components/client/FlightInfo.js
+++ src/components/client/FlightInfo.js
@@ -1,53 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import ClientNav from './ClientNav';
 
 export const FlightInfo = (props) => {
+    const [phone, setPhone] = useState("");
+
+    const handleOnChange = (e) => {
+        setPhone(e.target.value);
+    }
+
     return (
         <div>
             <ClientNav />
+            {/* Top Border */}
             <div class="hidden sm:block" aria-hidden="true">
-            <div class="py-5">
-                <div class="border-t border-gray-200"></div>
+                <div class="py-5">
+                    <div class="border-t border-gray-light"></div>
+                </div>
             </div>
-            </div>
-{/* Personal Information Section */}
+
+            {/* Personal Information Section */}
             <div class="mt-10 sm:mt-0">
             <div class="md:grid md:grid-cols-3 md:gap-6">
                 <div class="md:col-span-1">
-                <div class="px-4 sm:px-0">
-                    <h3 class="text-lg font-medium leading-6 text-gray-900">Personal information</h3>
+                    <div class="px-4 sm:px-0">
+                        <h3 class="text-lg font-medium leading-6 text-gray-darkest pl-4">Personal information</h3>
+                    </div>
                 </div>
-                </div>
-                <div class="mt-5 md:mt-0 md:col-span-2">
+            <div class="mt-5 md:mt-0 md:col-span-2">
                 <form action="#" method="POST">
                     <div class="shadow overflow-hidden sm:rounded-md">
                     <div class="px-4 py-5 bg-white sm:p-6">
                         <div class="grid grid-cols-6 gap-6">
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
-                            <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label for="first_name" class="block text-sm font-medium text-gray-dark">First name</label>
+                            <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                            <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label for="last_name" class="block text-sm font-medium text-gray-dark">Last name</label>
+                            <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                         <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium text-gray-700">Mobile phone</label>
-                            <input type="text" name="phone" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label class="block text-sm font-medium text-gray-dark">Mobile phone</label>
+                            <PhoneInput
+                                type="tel"
+                                name="phone"
+                                // Need to figure out why this class styling isn't working
+                                // class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray rounded-md"
+                                country={'us'}
+                                value={phone}
+                                handleOnChange={handleOnChange}
+                            />
                         </div>
 
                         <div class="col-span-6 sm:col-span-4">
-                            <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
-                            <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label for="email_address" class="block text-sm font-medium text-gray-dark">Email address</label>
+                            <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                     </div>
                 </div>
                     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-turquoise-dark hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-turquoise">
                             Save
                         </button>
                 </div>
@@ -57,10 +75,10 @@ export const FlightInfo = (props) => {
             </div>
             </div>
 
-{/* Arrival Flight Section */}
+            {/* Arrival Flight Section */}
             <div class="hidden sm:block" aria-hidden="true">
             <div class="py-5">
-                <div class="border-t border-gray-200"></div>
+                <div class="border-t border-gray-light"></div>
             </div>
             </div>
 
@@ -68,7 +86,7 @@ export const FlightInfo = (props) => {
             <div class="md:grid md:grid-cols-3 md:gap-6">
                 <div class="md:col-span-1">
                 <div class="px-4 sm:px-0">
-                    <h3 class="text-lg font-medium leading-6 text-gray-900">Arrival flight</h3>
+                    <h3 class="text-lg font-medium leading-6 text-gray-darkest pl-4">Arrival flight</h3>
                 </div>
                 </div>
                 <div class="mt-5 md:mt-0 md:col-span-2">
@@ -78,8 +96,8 @@ export const FlightInfo = (props) => {
                         <div class="grid grid-cols-6 gap-6">
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label class="block text-sm font-medium text-gray-700">Airport</label>
-                            <select name="airport" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <label class="block text-sm font-medium text-gray-dark">Airport</label>
+                            <select name="airport" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-turquoise focus:border-turquoise sm:text-sm">
                             <option>Liberia (LIR)</option>
                             <option>San Jose (SJO)</option>
                             <option>Other</option>
@@ -87,8 +105,8 @@ export const FlightInfo = (props) => {
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="country" class="block text-sm font-medium text-gray-700">Airline</label>
-                            <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <label for="country" class="block text-sm font-medium text-gray-dark">Airline</label>
+                            <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-turquoise focus:border-turquoise sm:text-sm">
                             <option>Delta</option>
                             <option>American</option>
                             <option>United</option>
@@ -96,25 +114,25 @@ export const FlightInfo = (props) => {
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label class="block text-sm font-medium text-gray-700">Flight number</label>
-                            <input type="text" name="flight" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label class="block text-sm font-medium text-gray-dark">Flight number</label>
+                            <input type="text" name="flight" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="last_name" class="block text-sm font-medium text-gray-700">Date</label>
-                            <input type="date" name="date" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label for="last_name" class="block text-sm font-medium text-gray-dark">Date</label>
+                            <input type="date" name="date" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                         <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium text-gray-700">Time</label>
-                            <input type="time" name="time" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label class="block text-sm font-medium text-gray-dark">Time</label>
+                            <input type="time" name="time" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                     </div>
                     </div>
 
                     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-turquoise-dark hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-turquoise">
                         Save
                         </button>
                     </div>
@@ -125,10 +143,10 @@ export const FlightInfo = (props) => {
             </div>
             </div>
 
-{/* Departure Flight Section */}
-<div class="hidden sm:block" aria-hidden="true">
+            {/* Departure Flight Section */}
+            <div class="hidden sm:block" aria-hidden="true">
             <div class="py-5">
-                <div class="border-t border-gray-200"></div>
+                <div class="border-t border-gray-light"></div>
             </div>
             </div>
 
@@ -136,7 +154,7 @@ export const FlightInfo = (props) => {
             <div class="md:grid md:grid-cols-3 md:gap-6">
                 <div class="md:col-span-1">
                 <div class="px-4 sm:px-0">
-                    <h3 class="text-lg font-medium leading-6 text-gray-900">Departure flight</h3>
+                    <h3 class="text-lg font-medium leading-6 text-gray-darkest pl-4">Departure flight</h3>
                 </div>
                 </div>
                 <div class="mt-5 md:mt-0 md:col-span-2">
@@ -146,8 +164,8 @@ export const FlightInfo = (props) => {
                         <div class="grid grid-cols-6 gap-6">
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label class="block text-sm font-medium text-gray-700">Airport</label>
-                            <select name="airport" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <label class="block text-sm font-medium text-gray-dark">Airport</label>
+                            <select name="airport" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-turquoise focus:border-turquoise sm:text-sm">
                             <option>Liberia (LIR)</option>
                             <option>San Jose (SJO)</option>
                             <option>Other</option>
@@ -155,8 +173,8 @@ export const FlightInfo = (props) => {
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="country" class="block text-sm font-medium text-gray-700">Airline</label>
-                            <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <label for="country" class="block text-sm font-medium text-gray-dark">Airline</label>
+                            <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-turquoise focus:border-turquoise sm:text-sm">
                             <option>Delta</option>
                             <option>American</option>
                             <option>United</option>
@@ -164,26 +182,26 @@ export const FlightInfo = (props) => {
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label class="block text-sm font-medium text-gray-700">Flight number</label>
-                            <input type="text" name="flight" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label class="block text-sm font-medium text-gray-dark">Flight number</label>
+                            <input type="text" name="flight" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="last_name" class="block text-sm font-medium text-gray-700">Date</label>
-                            <input type="date" name="date" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label for="last_name" class="block text-sm font-medium text-gray-dark">Date</label>
+                            <input type="date" name="date" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                         <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium text-gray-700">Time</label>
-                            <input type="time" name="time" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
+                            <label class="block text-sm font-medium text-gray-dark">Time</label>
+                            <input type="time" name="time" class="mt-1 focus:ring-turquoise focus:border-turquoise block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></input>
                         </div>
 
                     </div>
                     </div>
 
                     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Save
+                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-turquoise-dark hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-turquoise">
+                            Save
                         </button>
                     </div>
 
@@ -191,6 +209,12 @@ export const FlightInfo = (props) => {
                 </form>
                 </div>
             </div>
+            </div>
+            {/* Bottom Border */}
+            <div class="hidden sm:block" aria-hidden="true">
+                <div class="py-5">
+                    <div class="border-t border-gray-light"></div>
+                </div>
             </div>
 
         </div>
