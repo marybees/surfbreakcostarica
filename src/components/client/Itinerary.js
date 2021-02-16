30168f3864401e5734b98965f92cf3ab41268cbf
Mary Baker <meb601@gmail.com>
2021-02-15 19:57:41 -0500

    Add dynamic lesson date buttons to itinerary.

--- src/components/client/Itinerary.js
+++ src/components/client/Itinerary.js
@@ -3,6 +3,25 @@ import { connect } from 'react-redux'
 import ClientNav from './ClientNav';
 
 export const Itinerary = (props) => {
+
+    const clients = [{
+        first_name: "Mary",
+        last_name: "Baker",
+        email: "mary@mary.com",
+        level: "intermediate",
+        arrival_date: "05/03/2021",
+        departure_date: "05/06/2021",
+        lesson_count: 3,
+        notes: "Mary is beginner intermediate. She needs help catching waves on her own.",
+        lessons: [{date: "05/03/2021", time: "08:00 AM", location: "Tamarindo"}, {date: "05/04/2021", time: "07:00 AM", location: "Casitas"}, {date: "05/05/2021", time: "06:00 AM", location: "Avellanas"}]
+    }]
+
+    const lessons = clients.map((client) => {
+        return client.lessons.map((lesson) => {
+                return <a href="mailto:contact@surfbreakcostarica.com?subject=Please email me the e-sign release form" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-turquoise-light hover:bg-turquoise mx-1">{lesson.date}</a>
+        })
+    })
+
     return (
         <div>
             <ClientNav />
@@ -12,6 +31,11 @@ export const Itinerary = (props) => {
                     <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
                         <span class="block">Your Surf Itinerary</span>
                     </h2>
+                    <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+                        <div class="inline-flex rounded-md shadow mx-1">
+                            {lessons}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
